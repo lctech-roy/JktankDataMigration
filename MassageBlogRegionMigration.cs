a9afae75acfd8da88f7b921aa004de691222844a
lctech-roy <Roy@lctech.com.tw>
2023-11-15 15:22:07 +0800

    feat: MassageBlogRegionMigration add sorting index

--- MassageBlogRegionMigration.cs
+++ MassageBlogRegionMigration.cs
@@ -12,12 +12,14 @@ public class MassageBlogRegionMigration
     private const string QUERY_MASSAGE_CATEGORY = $@"SELECT a.""Id"" AS {nameof(MassageBlogRegion.Id)}, 
                                      a.""Name"" AS {nameof(MassageBlogRegion.Name)}
                                      FROM ""ArticleCategory"" a
-                                     WHERE a.""BoardId"" = 1128";
-    
-    private const string COPY_MASSAGE_BLOG_REGION_PREFIX = $"COPY \"{nameof(MassageBlogRegion)}\" (\"{nameof(MassageBlogRegion.Id)}\",\"{nameof(MassageBlogRegion.Name)}\"" + Setting.COPY_ENTITY_SUFFIX;
-    
+                                     WHERE a.""BoardId"" = 1128 AND a.""AdminOnly"" = false
+                                     ORDER BY ""SortingIndex""";
+
+    private const string COPY_MASSAGE_BLOG_REGION_PREFIX = $"COPY \"{nameof(MassageBlogRegion)}\" (\"{nameof(MassageBlogRegion.Id)}\",\"{nameof(MassageBlogRegion.Name)}\",\"{nameof(MassageBlogRegion.SortingIndex)}\""
+                                                         + Setting.COPY_ENTITY_SUFFIX;
+
     private const string MASSAGE_BLOG_REGION_PATH = $"{Setting.INSERT_DATA_PATH}/{nameof(MassageBlogRegion)}";
-    
+
     public void Migration()
     {
         using var cn = new NpgsqlConnection(Setting.NEW_FORUM_CONNECTION);
@@ -27,12 +29,14 @@ public class MassageBlogRegionMigration
         var dateNow = DateTimeOffset.UtcNow;
 
         var massageBlogRegionSb = new StringBuilder();
-        
+
+        var sortingIndex = 0;
+
         foreach (var massageBlogRegion in massageBlogRegions)
         {
-            massageBlogRegionSb.AppendValueLine(massageBlogRegion.Id, massageBlogRegion.Name, dateNow, 0, dateNow, 0, 0);
+            massageBlogRegionSb.AppendValueLine(massageBlogRegion.Id, massageBlogRegion.Name, ++sortingIndex, dateNow, 0, dateNow, 0, 0);
         }
-        
+
         FileHelper.WriteToFile(MASSAGE_BLOG_REGION_PATH, $"{nameof(MassageBlogRegion)}.sql", COPY_MASSAGE_BLOG_REGION_PREFIX, massageBlogRegionSb);
     }
 }
