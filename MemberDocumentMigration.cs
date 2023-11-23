55634d5c933c61cab17f7dfd0895c463d01d42cf
lctech-roy <Roy@lctech.com.tw>
2023-11-23 14:26:57 +0800

    feat: 調整memberDocumentMigration

--- MemberDocumentMigration.cs
+++ MemberDocumentMigration.cs
@@ -1,44 +1,47 @@
 using Dapper;
 using JKTankDataMigration.Helpers;
-using Lctech.JKTank.Core.Domain.Documents;
+using Lctech.JKTank.Core.Domain.Entities;
+using Lctech.JKTank.Core.Domain.Enums;
 using Nest;
 using Npgsql;
+using Member = Lctech.JKTank.Core.Domain.Documents.Member;
 
 namespace JKTankDataMigration;
 
-public class MemberDocumentMigration
+public class MemberDocumentMigration(IElasticClient elasticClient)
 {
-    private readonly IElasticClient _elasticClient;
-    private readonly string _elasticIndex;
+    private readonly string _elasticIndex = ElasticHelper.GetMemberIndex(Setting.LOOK_ES_INDEX);
     private static readonly Dictionary<long, long[]> UserRoleDic = LookAuthHelper.GetLookAuthUserRoleDic();
 
-    private const string QUERY_LOOK_MEMBER_SQL = $@"SELECT m.""Id"",m.""PrivacyType"",
-                                                    ms.""HotScore"",
-                                                    ms.""ViewCount"",
-                                                    ms.""ObtainDonateCount"",
-                                                    ms.""ObtainPurchaseCount"",
-                                                    ms.""ObtainDonateJPoints"",
-                                                    ms.""ObtainPurchaseJPoints"",
-                                                    ms.""ObtainTotalJPoints"",
-                                                    ms.""ActualObtainDonateJPoints"",
-                                                    ms.""ActualObtainPurchaseJPoints"",
-                                                    ms.""ActualObtainTotalJPoints"",
-                                                    ms.""CommentCount"",
-                                                    ms.""ReactCount"",
-                                                    ms.""FavoriteCount"",
-                                                    ms.""FollowerCount"",
-                                                    ms.""MassageBlogCount"",
-                                                    ms.""LinkMassageBlogCount"",
-                                                    ms.""PricingBlogCount"",
-                                                    ms.""TotalBlogCount""
-                                                    FROM ""Member"" m
-                                                    LEFT JOIN ""MemberStatistic"" ms ON ms.""Id"" = m.""Id""";
-    
-    public MemberDocumentMigration(IElasticClient elasticClient)
-    {
-        _elasticClient = elasticClient;
-        _elasticIndex = ElasticHelper.GetMemberIndex(Setting.LOOK_ES_INDEX);
-    }
+    private const string QUERY_LOOK_MEMBER_SQL = $"""
+                                                   SELECT m."Id",
+                                                          m."{nameof(Member.PrivacyType)}",
+                                                          ms."{nameof(MemberStatistic.HotScore)}",
+                                                          ms."{nameof(MemberStatistic.ViewCount)}",
+                                                          ms."{nameof(MemberStatistic.ObtainDonateCount)}",
+                                                          ms."{nameof(MemberStatistic.ObtainPurchaseCount)}",
+                                                          ms."{nameof(MemberStatistic.ActualObtainDonateJPoints)}",
+                                                          ms."{nameof(MemberStatistic.ActualObtainPurchaseJPoints)}",
+                                                          ms."{nameof(MemberStatistic.ActualObtainTotalJPoints)}",
+                                                          ms."{nameof(MemberStatistic.ObtainDonateJPoints)}",
+                                                          ms."{nameof(MemberStatistic.ObtainPurchaseJPoints)}",
+                                                          ms."{nameof(MemberStatistic.ObtainTotalJPoints)}",
+                                                          ms."{nameof(MemberStatistic.PurchaseCount)}",
+                                                          ms."{nameof(MemberStatistic.DonateJPoints)}",
+                                                          ms."{nameof(MemberStatistic.PurchaseJPoints)}",
+                                                          ms."{nameof(MemberStatistic.ConsumeTotalJPoints)}",
+                                                          ms."{nameof(MemberStatistic.CommentCount)}",
+                                                          ms."{nameof(MemberStatistic.ReactCount)}",
+                                                          ms."{nameof(MemberStatistic.FavoriteCount)}",
+                                                          ms."{nameof(MemberStatistic.FollowerCount)}",
+                                                          ms."{nameof(MemberStatistic.MassageBlogCount)}",
+                                                          ms."{nameof(MemberStatistic.LinkMassageBlogCount)}",
+                                                          ms."{nameof(MemberStatistic.PricingBlogCount)}",
+                                                          ms."{nameof(MemberStatistic.TotalBlogCount)}",
+                                                          m."{nameof(Member.CreationDate)}"
+                                                          FROM "Member" m
+                                                          LEFT JOIN "MemberStatistic" ms ON ms."Id" = m."Id"
+                                                   """;
 
     public async Task MigrationAsync(CancellationToken cancellationToken = new())
     {
@@ -61,7 +64,7 @@ public class MemberDocumentMigration
         {
             await CommonHelper.WatchTimeAsync
                 (
-                 $"{nameof(_elasticClient.BulkAsync)}({Setting.LOOK_ES_BATCH_SIZE}) offset:{offset}",
+                 $"{nameof(elasticClient.BulkAsync)}({Setting.LOOK_ES_BATCH_SIZE}) offset:{offset}",
                  async () =>
                  {
                      var length = offset + Setting.LOOK_ES_BATCH_SIZE;
@@ -69,7 +72,7 @@ public class MemberDocumentMigration
                      if (length > documents.Length)
                          length = documents.Length;
 
-                     var response = await _elasticClient.BulkAsync(descriptor => descriptor.IndexMany(documents[offset..length], (des, doc) => des.Index(_elasticIndex)
+                     var response = await elasticClient.BulkAsync(descriptor => descriptor.IndexMany(documents[offset..length], (des, doc) => des.Index(_elasticIndex)
                                                                                                                                                   .Id(doc.Id)
                                                                                                                                                   .Document(doc)), cancellationToken);
 
